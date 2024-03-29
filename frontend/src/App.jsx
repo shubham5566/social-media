6f218d3f08de4efc15a3688f89c6e4167270728b
shubham bhavsar <shubhambhavsar5566@gmail.com>
2024-03-29 12:47:53 +0530

    bacis set up for UI

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import Header from './componants/header/Header';
+
+
+function App() {
+  
+
+  return (
+    <>
+    <Header></Header>
+    </>
+  )
+}
+
+export default App
